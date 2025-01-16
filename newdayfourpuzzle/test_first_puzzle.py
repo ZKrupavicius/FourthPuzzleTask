fac26b2b16627fca926dc7c9c14dd7e5e2b154f9
ZKrupavicius <zaimutis@gmail.com>
2025-01-16 14:20:54 +0200

    More test cases

--- newdayfourpuzzle/test_first_puzzle.py
+++ newdayfourpuzzle/test_first_puzzle.py
@@ -1,3 +1,5 @@
+import pytest
+
 from firstpuzzle import count_words_algorithm, count_xmas_words, make_map, read_file
 
 
@@ -13,6 +15,45 @@ def test_count_of_words():
     assert count_words_algorithm(x, y, map_) == 2, "Incorrect amount of MAS words with given coordinates"
 
 
+def test_wrong_x_input():
+    x = "A"
+    y = 3
+    map_ = [
+        ['M', 'X', 'M', 'A', 'S'],
+        ['X', 'X', 'M', 'X', 'X'],
+        ['S', 'S', 'S', 'A', 'S'],
+        ['X', 'M', 'A', 'S', 'S']
+    ]
+    with pytest.raises(Exception):
+        count_words_algorithm(x, y, map_)
+
+
+def test_wrong_map_input():
+    x = 4
+    y = 3
+    map_ = [
+        ['M', 'X', 'M', 'A', 'S'],
+        [1, 'X', 'M', 'X', 'X'],
+        ['S', 'S', 'S', 'A', 'S'],
+        ['X', 'M', 'A', 'S', 'S']
+    ]
+    with pytest.raises(Exception):
+        count_words_algorithm(x, y, map_)
+
+
+def test_out_of_bounds_coordinates():
+    x = 0
+    y = 8
+    map_ = [
+        ['M', 'X', 'M', 'A', 'S'],
+        ['X', 'X', 'M', 'X', 'X'],
+        ['S', 'S', 'S', 'A', 'S'],
+        ['X', 'M', 'A', 'S', 'S']
+    ]
+    with pytest.raises(Exception):
+        count_words_algorithm(x, y, map_)
+
+
 def test_count_xmas():
     map_ = [
         ['M', 'X', 'M', 'A', 'S'],
@@ -23,15 +64,32 @@ def test_count_xmas():
     assert count_xmas_words(map_) == 3, "Incorrect count of XMAS words"
 
 
+def test_wrong_map():
+    map_ = [
+        ['M', 'X', 'A'],
+        ['X', 'X', 'X']
+    ]
+    with pytest.raises(Exception):
+        count_xmas_words(map_)
+
+
+def test_empty_map():
+    map_ = []
+    with pytest.raises(Exception):
+        count_xmas_words(map_)
+
+
 def test_make_map():
     first_map = ['MASXA', 'XXMAS']
     result_map = [['M', 'A', 'S', 'X', 'A'], ['X', 'X', 'M', 'A', 'S']]
     assert make_map(first_map) == result_map, "Incorrect creation of the map"
 
+
 def test_make_from_file_to_list():
     text_file = "input.txt"
     assert type(read_file(text_file)) == list, "Return is not a list"
 
-def test_if_file_empty():
+
+def test_is_file_empty():
     text_file = "input.txt"
-    assert len(read_file(text_file)) != 0, "File is empty"
+    assert len(read_file(text_file)) > 0, "File is empty"
